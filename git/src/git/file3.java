2d8589779a0e396b49776baa4b6690ee1b31d00f
HaneeshaRongala <51309563+HaneeshaRongala@users.noreply.github.com>
2019-06-17 17:39:19 +0530

    Update file3.java

--- git/src/git/file3.java
+++ git/src/git/file3.java
@@ -2,8 +2,9 @@ package git;
 
 public class file3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 System.out.println("hi");
 	}
 
